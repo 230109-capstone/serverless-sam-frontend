bbc7d7a3d89d3fc35d5c9983c76cbbac05ec88c9
kfarruh77 <kfarruh77@gmail.com>
2023-03-07 14:45:32 -0500

    Added Filter Functionality

--- src/Components/View-Reimbursement/ViewReimbursements.tsx
+++ src/Components/View-Reimbursement/ViewReimbursements.tsx
@@ -1,7 +1,17 @@
+import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import React, { useEffect, useMemo, useState } from 'react'
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/Store'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+
 
 
 interface ErrorType {
@@ -22,7 +32,7 @@ function ViewReimbursements() {
     const fetchTickets = async () =>{
       setFetch({...fetch, loading: true});
       try {
-        const result = await axios.get(`bdx5a9kkg3.execute-api.us-east-1.amazonaws.com/Prod/reimbursements`, {headers: {
+        const result = await axios.get(`http://127.0.0.1:8080/reimbursements`, {headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }});
         setFetch({loading:false, error: false, message: ""})
@@ -37,45 +47,63 @@ function ViewReimbursements() {
   }, [])
   
 
-  //  const filteredTickets = useMemo(() => {
-  //       if (!filter) {
-  //           return tickets;
-  //       }
-  //       return tickets.filter((ticket) => ticket.status.includes(filter));
-  //   }, [filter, tickets]);
+   const filteredTickets = useMemo(() => {
+        if (!filter) {
+            return tickets;
+        }
+        return tickets.filter((ticket) => ticket.status.includes(filter));
+    }, [filter, tickets]);
 
+
+   function handleFilter(event: React.MouseEvent<HTMLInputElement>) {
+      setFilter(event.currentTarget.value);
+   }
 
 
 
   return (
-    <div>
+    <Container fluid>
       {
         fetch.loading ? <h1>Loading</h1> :
         fetch.error ? <h1>{fetch.message}</h1> :
-        <div>
+        <>
+        <Container>
+          <DropdownButton id="dropdown-item-button" title="Filter">
+              <Dropdown.Item as="button" onClick={handleFilter} value='pending'>Pending</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={handleFilter} value='approved'>Approved</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={handleFilter} value='denied'>Denied</Dropdown.Item>
+          </DropdownButton>
+        </Container>
+        <Row style={{ minWidth: "50vw" }}>
           {
-            tickets.map(ticket => (
-              <ul key={ticket.id}>
-                <li><img src={'s3bucketlink'} alt={ticket.status}/></li>
-                <li>{ticket.description}</li>
-                <li>{ticket.amount}</li>
-                <li>{ticket.status}</li>
-                <li>{ticket.submitter}</li>
-                {
-                  User.role === 'finance_manager' && ticket.status === 'pending' ? 
-                  <li>
-                    <button >Approve</button>
-                    <button >Deny</button>
-                  </li> :
-                  <></>
-                }
-              </ul>
+            filteredTickets.map(ticket => (
+              <Col key={ticket.id}>
+                <Card style={{ width: '18rem' }} >
+                  <Card.Img variant="top" src={'s3bucketlink'} alt={ticket.status}/>
+                  <Card.Body>
+                    <Card.Text>{ticket.description}</Card.Text>
+                  </Card.Body>
+                  <ListGroup className="list-group-flush">
+                    <ListGroup.Item>{`$${ticket.amount}`}</ListGroup.Item>
+                    <ListGroup.Item>{ticket.status}</ListGroup.Item>
+                    <ListGroup.Item>{ticket.submitter}</ListGroup.Item>
+                  </ListGroup>
+                  {
+                    User.role === 'finance_manager' && ticket.status === 'pending' ? 
+                    <Card.Body>
+                      <Button variant='success' >Approve</Button>
+                      <Button variant='danger'>Deny</Button>
+                    </Card.Body> :
+                    <></>
+                  }
+                </Card>
+              </Col>
             ))
           }
-          
-        </div>
+        </Row>
+        </>
       }
-    </div>
+    </Container>
   )
 }
 
