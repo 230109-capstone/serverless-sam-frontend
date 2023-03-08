327e3b1e96e4db07cde5f782fe7a8a9946fe48ff
Christopher C <83333157+ChristopherC360@users.noreply.github.com>
2023-03-08 14:48:56 -0600

    Update URL.ts

--- src/models/URL.ts
+++ src/models/URL.ts
@@ -1,1 +1,1 @@
-export const remoteUrl: string = "https://3z3bsyt5aa.execute-api.us-east-1.amazonaws.com/Prod";
+export const remoteUrl: string = "http://3z3bsyt5aa.execute-api.us-east-1.amazonaws.com/Prod";
