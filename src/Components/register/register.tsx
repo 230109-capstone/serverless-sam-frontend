b312b64b00afc250dbbe8bdf2d10fb2c5744f507
mcmillenchrisj <mcmillenchrisj@gmail.com>
2023-03-10 11:26:05 -0500

    error messages appear correctly

--- src/Components/register/register.tsx
+++ src/Components/register/register.tsx
@@ -1,22 +1,13 @@
 import React, { useState } from "react";
-import { LoginUser } from "../../models/User";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import './register.css'
-import { error } from "console";
 import { remoteUrl } from '../../models/URL'
 
 function Register() {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
-    const config = {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'POST',
-            'Access-Control-Allow-Headers': 'Content-Type'
-        }
-    };
+
     const data = {
         "username": username,
         "password": password
@@ -24,16 +15,20 @@ function Register() {
     let navigate = useNavigate();
 
     async function registerSubmit() {
-        const response = await axios.post(remoteUrl + '/users', data, config);
-        try{
-            if (response.status === 200) {
-              alert('Successfully Registered');
-              navigate('/');
-          }
-        }catch(error) {
-            alert(error);
-          };
-        };
+       
+            try { 
+                const response = await axios.post(remoteUrl + '/users', data);
+                console.log(response);
+                if (response.status === 200) {
+                    alert(response.data.message);
+                    navigate('/');
+                } else {
+                    alert(response.data.message);
+                }
+            } catch(error: any) {
+                alert(error.response.data.errors);
+            };
+        }
     
 
     return ( 
